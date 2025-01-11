94ed2bda2476342d3e967dbd0061b39f538aa91f
4llower <i.4llower@gmail.com>
2025-01-12 00:07:24 +0100

    Made approach with foreign data wrapper and refined OLTP with data type

---
+++ olap/init_foreign_data_wrapper.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
+
+CREATE SERVER IF NOT EXISTS oltp_server
+  FOREIGN DATA WRAPPER postgres_fdw
+  OPTIONS (host 'localhost', dbname 'oltp_db', port '5432');
+
+CREATE USER MAPPING IF NOT EXISTS FOR current_user
+SERVER oltp_server
+OPTIONS (
+  user 'postgres',
+  password 'postgres'
+);
+
+CREATE SCHEMA IF NOT EXISTS fdw_oltp;
+
+IMPORT FOREIGN SCHEMA public LIMIT TO (Doctors, Patients, Appointments, Billing)
+  FROM SERVER oltp_server
+  INTO fdw_oltp;
